350b2730a11aaad3fa35eb9b87ad8b6a36133dc5
akinao76 <jjo12137@gmail.com>
2025-03-31 11:51:51 +0900

    flask frame mk.1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, app, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+   
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        file.save(f'uploads/{file.filename}')
+        return render_template('index.html', message='File uploaded successfully')
+    else:
+        return render_template('index.html', error='No file selected')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
